b635510a739e7468fc0ecfc95232d70cf51e6072
Benjaminroy2003 <2003benjaminroy@gmail.com>
2025-08-25 23:39:41 +0530

    added few stuff

---
+++ image_to_text.py
@@ -0,0 +1,15 @@
+class image_to_text():
+    def model(self):
+        # model initialization
+        pass
+
+    def image_type_prompt(self):
+        # handle different images
+        pass
+
+    def image_classification(self):
+        # classify images
+        pass
+
+    
+    
---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+    #1) input image dir 
+    #2) classify the type of image
+    #3) extract information from the image 
+    #4)combine the image information with the text information
+    #5) store it in a database
+    pass
